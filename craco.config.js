2d911f9bb8a652d05bf1a8cd6f356fee87fabcef
Huy Dang <dqhuy7@gmail.com>
2021-06-20 18:36:32 +0700

    font

--- craco.config.js
+++ craco.config.js
@@ -7,7 +7,9 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { "@primary-color": "#7367f0" },
+            modifyVars: {
+              "@primary-color": "#7367f0",
+            },
             javascriptEnabled: true,
           },
         },
