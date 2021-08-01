1aaf72204ee8b9393cfba460813ddc9b39a56bea
Huy Dang <dqhuy7@gmail.com>
2021-08-01 13:52:02 +0700

    add charka ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./styles/index.scss";
 import "./App.scss";
 import AppRoute from "./routes/AppRoute";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="app">
-      <AppRoute />
+      <ChakraProvider>
+        <AppRoute />
+      </ChakraProvider>
     </div>
   );
 }
