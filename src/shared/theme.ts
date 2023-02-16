8adc6b5ce9a9276bb5771ccaab6e5ca5d829314d
huydang7 <dqhuy7@gmail.com>
2023-02-16 23:22:01 +0700

    remove poppins fonts

--- src/shared/theme.ts
+++ src/shared/theme.ts
@@ -2,7 +2,7 @@ import { ThemeConfig } from "antd/es/config-provider/context";
 
 export const theme: ThemeConfig = {
   token: {
-    fontFamily: "Poppins",
+    fontFamily: "Inter",
     colorPrimary: "#1a73e8",
     colorLink: "#1a73e8",
   },
